bdefbf96f9cb197e8a1bbdb8566a45bb4b9d92ed
AnjaliG927 <anjali.ghaligi@gmail.com>
2020-07-15 11:57:50 -0400

    updated part 5 sql

--- Part5.sql
+++ Part5.sql
@@ -3,7 +3,7 @@
 
 SELECT CONCAT(emp.lastName,' ',emp.firstName) AS 'Sales_Rep',
 COUNT(orderD.quantityOrdered) AS '#_Orders',
-IFNULL((orderD.quantityOrdered * orderD.priceEach), 0) AS 'Total_Sales'
+IFNULL((sum(orderD.quantityOrdered * orderD.priceEach)), 0) AS 'Total_Sales'
 FROM employees emp 
 left JOIN customers cus ON emp.employeeNumber=cus.salesRepEmployeeNumber
 left JOIN orders orde ON cus.customerNumber=orde.customerNumber
